63f8ec967d9beeac500bb9ad1b43381b9a14dfe4
julietteseive <julietteseive@gmail.com>
2016-04-12 18:48:10 -0500

    moving files, added authentication

---
+++ auth.py
@@ -0,0 +1,11 @@
+
+class TwitterAuth:
+	# Go to http://dev.twitter.com and create an app. 
+	# The consumer key and secret will be generated for you after
+	consumer_key="ICSEnI77bbnY5cWe4vCMDINIt"
+	consumer_secret="OZEdFLMfN9UVo8bg2NLGY3DQmrW3AvL7Q4TuN8s4k4BDfnUtDC"
+
+	# After the step above, you will be redirected to your app's page.
+	# Create an access token under the the "Your access token" section
+	access_token="181293710-NHe5RNKmvA6AYGiZR4hBKRbBExQU3LcKtEOcoIhw"
+	access_token_secret="LJHLqrBnm0wbmnn0UHaR6N5ygC9f5pYvCKeLU3UB2XC8c"
